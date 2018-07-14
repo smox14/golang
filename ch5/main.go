480d2ba36b6b25a4bb279544322c5b5a7c461b37
Mukkii <Mukkii@MacBook.local>
2018-07-14 11:02:20 +0700

    add if statement

--- ch5/main.go
+++ ch5/main.go
@@ -2,8 +2,17 @@ package main
 import "fmt"
 
 func main(){
-	for i:=1; i <= 10; i++ {
-		fmt.Println(i)
 
-	}
+	forloop()
+
+}
+
+func forloop(){
+	for i:=1; i <= 10; i++ {
+		if i%2 == 0{
+		fmt.Println(i,"Even")
+		} else {
+		fmt.Println(i,"odd")
+		}
+  }
 }
