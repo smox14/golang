f2727c8d3f0741905d1c86cbe1ce79935535d1df
Mukkii <smox14.dev@gmail.com>
2018-07-21 15:24:37 +0700

    update

--- ch6/maps.go
+++ ch6/maps.go
@@ -23,23 +23,23 @@ func main() {
 
 	elements := map[string]map[string]string{
 		"H": map[string]string{
-			 "name": "Hydrogen",
-			 "state": "gas",
+			"name":  "Hydrogen",
+			"state": "gas",
 		},
 		"He": map[string]string{
-			"name": "Helium",
+			"name":  "Helium",
 			"state": "gas",
-		 },
+		},
 		"Li": map[string]string{
-			"name": "Lithium",
+			"name":  "Lithium",
 			"state": "solid",
 		},
 		"Be": map[string]string{
-			"name": "Beryllium",
+			"name":  "Beryllium",
 			"state": "solid",
 		},
 		"B": map[string]string{
-			"name": "Boron",
+			"name":  "Boron",
 			"state": "solid",
 		},
 		"C": map[string]string{
@@ -47,28 +47,28 @@ func main() {
 			"state": "solid",
 		},
 		"N": map[string]string{
-			"name": "Nitrogen",
+			"name":  "Nitrogen",
 			"state": "gas",
 		},
-		"O": map[string]string {
-			"name": "Oxygen",
+		"O": map[string]string{
+			"name":  "Oxygen",
 			"state": "gas",
 		},
-		"F": map[string]string {
-			"name": "Fluorine",
+		"F": map[string]string{
+			"name":  "Fluorine",
 			"state": "gas",
 		},
-		"Ne": map[string]string {
-			"name": "Neon",
+		"Ne": map[string]string{
+			"name":  "Neon",
 			"state": "gas",
-		},	
+		},
 	}
-	
+
 	fmt.Println(elements["Ne"])
 
 	/* return two values.
-		the first value is the result of the lookup
-		the second tells us whether or not the lookup was successful
+	the first value is the result of the lookup
+	the second tells us whether or not the lookup was successful
 	*/
 
 	//name, ok := elements["Un"]
@@ -77,6 +77,5 @@ func main() {
 	if name, ok := elements["Un"]; ok {
 		fmt.Println(name, ok)
 	}
-
 
 }
