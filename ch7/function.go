6d4da1dcbdd6e371869cc187e7899a22a8b720af
Mukkii <smox14.dev@gmail.com>
2018-07-26 16:37:12 +0700

    modified function

--- ch7/function.go
+++ ch7/function.go
@@ -13,4 +13,21 @@ func main() {
 	xs := []float64{98, 93, 77, 82, 83}
 	fmt.Println(average(xs))
 
+	fmt.Println(f1())
+
+	x, y := f()
+	fmt.Println(x, y)
+}
+
+func f() (int, int) {
+	return 5, 6
+}
+
+func f1() int {
+	return f2()
+}
+
+func f2() (r int) {
+	r = 1
+	return
 }
